c8ec193637c00cbb3c040ec98b51a312d6b928da
Vinay Soni <namitjairaj23@gmail.com>
2025-09-16 15:40:22 +0530

    test

--- Goverment/schems/src/app/components/landing/landing.component.ts
+++ Goverment/schems/src/app/components/landing/landing.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule] 
 })
 export class LandingComponent {
   sidebarOpen = false;
